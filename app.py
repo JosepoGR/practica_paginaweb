9d982e108fa91676a329e3de77363ea789fd0cad
josepoGR <gutierrespablo22@gmail.com>
2024-02-22 14:00:16 -0600

    agregando archivos 3

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
+from flask import Flask, render_template
 import sqlite3
 
-# Conectar a la base de datos (asegúrate de tener el archivo SQLite presente)
-conn = sqlite3.connect('estudiantes.db')
+app = Flask(__name__)
 
-# Crear un cursor para ejecutar comandos SQL
-cursor = conn.cursor()
+@app.route('/')
+def mostrar_estudiantes():
+    # Conectar a la base de datos
+    conn = sqlite3.connect('estudiantes.db')
+    cursor = conn.cursor()
 
-# Nombre de la tabla que quieres conectar
-nombre_tabla = 'estudiantes'
+    # Nombre de la tabla que quieres conectar
+    nombre_tabla = 'estudiantes'
 
-# Consultar datos de la tabla
-cursor.execute(f"SELECT * FROM {nombre_tabla}")
-filas = cursor.fetchall()
+    # Consultar datos de la tabla
+    cursor.execute(f"SELECT * FROM {nombre_tabla}")
+    filas = cursor.fetchall()
 
-# Imprimir resultados
-print(f"Contenido de la tabla {nombre_tabla}:")
-for fila in filas:
-    print(f"ID: {fila[0]}, nombre1: {fila[1]}, nombre2: {fila[2]}, apellido1: {fila[3]}, apellido2: {fila[4]} telefono: {fila[5]}")
+    # Cerrar la conexión
+    conn.close()
 
-# Cerrar la conexión
-conn.close()
+    # Renderizar la plantilla HTML con los datos obtenidos
+    return render_template('index.html', filas=filas)
+
+if __name__ == 'main':
+    app.run(debug=True)
